bd12b00a3e6b20c1e83777f752f1c81374db4cc9
Hasna Hena Mow <hasna.hena@welldev.io>
2026-02-23 02:09:14 +0600

    chore: fastapi app with async db connection to postgres
    docker postgres setup:
    1. Pull postgres image: "docker pull postgres"
    2. Create volume for persistent data: "docker create volume nlf_data"
    3. Run the container attaching the volume: "docker run --name postgres_db -e POSTGRES_PASSWORD=secretpass -p 5432:5432 -v nlf_data:/var/lib/postgresql/ -d postgres"

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from backend!")
+import uvicorn
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from app.core.database import engine, Base
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+def root():
+    return {"message": "Application is running!"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8080,
+        reload=True,
+    )
