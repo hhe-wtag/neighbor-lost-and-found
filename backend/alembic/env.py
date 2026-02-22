74f0daef3e41716c87241dd8617452813d0c90ea
Hasna Hena Mow <hasna.hena@welldev.io>
2026-02-23 05:43:44 +0600

    chore: alembic migration to create user table
    1. alembic revision --autogenerate -m "initial schema only users exists
    2. alembic upgrade head

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,6 +9,7 @@ from alembic import context
 
 from app.core.config import settings
 from app.core.database import Base
+from app.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
