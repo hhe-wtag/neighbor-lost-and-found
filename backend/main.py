e19495705fcd178e49a713786f8f485ee91ae198
Hasna Hena Mow <hasna.hena@welldev.io>
2026-02-23 08:06:26 +0600

    chore: basic CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 import uvicorn
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.api.v1.api import api_router as api_v1_router
 from app.core.database import engine
@@ -14,6 +16,14 @@ async def lifespan(_app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(LoggingMiddleware)
 
